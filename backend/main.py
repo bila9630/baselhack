5946cc031cdfb3b7f46e2336f87a01f4400cc35f
BAAAKA <baaakaDev@protonmail.ch>
2024-11-01 23:30:14 +0100

    backend structure

---
+++ backend/main.py
@@ -0,0 +1,6 @@
+"""
+openai_client for the communcation
+gonna make something like prediction_clustering.py, use userdata and metadata to predict answers and make recommendation to the user in real time.
+"""
+
+
